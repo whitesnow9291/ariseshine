8e5d9e802edfb60ad9e2e696ece69b4ddfee9e9d
whitesnow <whitesnow9291@yahoo.com>
2017-04-04 22:32:56 +0800

    business link removed

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017/ariseshine' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-  //'url': 'mongodb://oliver.heldens0405:oliverheldens@ds013941.mlab.com:13941/heroku_hsffft92'
+	//'url' : 'mongodb://localhost:27017/ariseshine' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+  'url': 'mongodb://oliver.heldens0405:oliverheldens@ds013941.mlab.com:13941/heroku_hsffft92'
 };
